84bfb5be8d3f9d66f8f6adbde9623e477161bfa6
Devajit <patar1447@gmail.com>
2024-09-01 15:40:38 +0530

    Thunder Client Setup for end point testing

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("Server")
+const express = require('express');
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Response from server");
+});
+
+app.get("/api/contacts",(req,res)=>{
+    // res.send("Get All Contacts");
+    res.status(200).json({contacts:["9811123456","8723573456","8934265789"]});
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server run at port ${PORT}`);
+});

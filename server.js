f3ff175ab200be323f3b46a4594793a8645036e2
Devajit <patar1447@gmail.com>
2024-09-08 13:09:58 +0530

    Exprtes Async Handler

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorhandler');
 const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 
 app.listen(PORT,()=>{
